6560743979b1e559616bdda006fba6c754ab87bb
AlexanderVorvul <80675797+AlexanderVorvul@users.noreply.github.com>
2021-10-12 10:06:52 +0300

    feat: implement Stack class

--- src/st-stack.js
+++ src/st-stack.js
@@ -13,18 +13,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
  module.exports = class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor() { this.lifo =  new Array();}
+
+  push( element ) {
+    this.lifo.push(element);    
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.lifo.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.lifo[this.lifo.length -1];
   }
 }
